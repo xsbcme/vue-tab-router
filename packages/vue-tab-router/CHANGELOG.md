dab2ad759972e2bd50d5ff926db512aac7faf38f
xsbcme <2974550071@qq.com>
2025-11-25 11:25:11 +0800

    chore

--- packages/vue-tab-router/CHANGELOG.md
+++ packages/vue-tab-router/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @xsbcme/vue-tab-router
 
+## 1.0.0-beta.19
+
+### Patch Changes
+
+- chore
+
 ## 1.0.0-beta.18
 
 ### Patch Changes
--- packages/vue-tab-router/src/components/dynamic-icon.vue
+++ packages/vue-tab-router/src/components/dynamic-icon.vue
@@ -162,7 +162,7 @@ async function processSvgString(svgString: string) {
 .dynamic-icon {
     width: 100%;
     height: 100%;
-    display: flex;
+    display: inline-flex;
     align-items: center;
     justify-content: center;
 
