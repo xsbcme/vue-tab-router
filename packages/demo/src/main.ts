2ed9ba5a8aa24c376f97bccfe554d510357850c7
xsbcme <2974550071@qq.com>
2026-06-15 10:13:49 +0800

    chore:浏览器滚动条默认样式更改。

--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -4,10 +4,16 @@ import App from "@/App.vue";
 import arco from "@/plugins/arco";
 import store from "@/plugins/store";
 import vueRouter from "@/plugins/vue-router";
+import scrollbar from "@/plugins/scrollbar";
 import tabRouter from "@/plugins/tab-router";
 
 const run = async () => {
-  const app = createApp(App).use(arco).use(store).use(vueRouter).use(tabRouter);
+  const app = createApp(App)
+    .use(arco)
+    .use(store)
+    .use(vueRouter)
+    .use(scrollbar)
+    .use(tabRouter);
   await vueRouter.isReady();
   app.mount("#app");
 };
